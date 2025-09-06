34ed4e9b55fb456a3141a7123c2430539f451518
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2025-09-06 18:23:46 +0800

    Create a function named getComputerChoice, and make it return a random value from the choices array

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const choices = ["rock", "paper", "scissors"];
+
+// create a function named getComputerChoice
+function getComputerChoice() {
+    // create a var named random and randomly assign it a value in choices
+    let random = Math.floor(Math.random() * choices.length);
+    // return the random value in choices
+    return choices[random];
+};
